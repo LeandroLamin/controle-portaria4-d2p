b7a02609581205e198ebbb7759224b58bfebc2a5
LeandroLamin <160440224+LeandroLamin@users.noreply.github.com>
2026-03-30 14:37:50 -0300

    Update config.js

--- conexao/config.js
+++ conexao/config.js
@@ -1,1 +1,7 @@
+// conexao/config.js
+// ============================================================
+// CONFIGURAÇÃO GLOBAL DO SISTEMA D2P
+// Altere apenas aqui caso a URL do servidor mude
+// ============================================================
 
+const N8N_URL = 'https://n8n.laminlpp.com.br/webhook';
